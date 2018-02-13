898dbc69bd44649669785c6d224c262a7a462e3f
poitras <nicopoitras@gmail.com>
2018-02-12 23:37:26 -0500

    added weather data to reducers

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,16 @@
+import { FETCH_WEATHER } from '../actions/index'
+
 export default function(state = null, action ) {
   // The promise received from the action is open for us by the middleware
   // console.log('Action received', action);
+  console.log(">>>>", action.payload)
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // to make sure were not mutating the state we concat
+      // return state.concat([action.payload.data])
+      // better with es6
+      return [ state.payload.data, ...state ]
+  }
   
   return state
 }

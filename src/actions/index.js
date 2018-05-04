9714a6104e31a30af3140d0e04f6b9ac5cc9da96
poitras <nicopoitras@gmail.com>
2018-05-03 22:42:01 -0400

    feat: separated chart view into dummy component + pressure chart added

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,10 +8,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`
   const request = axios.get(url)
-
-  // console.log('Request', request)
   
-
   return {
     type: FETCH_WEATHER,
     payload: request
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Sparklines, SparklinesLine } from 'react-sparklines'
+import Chart from '../components/charts'
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,14 +8,16 @@ class WeatherList extends Component {
     
     const name = cityData.city.name
     const temps = cityData.list.map(weather => weather.main.temp)
+    const pressures = cityData.list.map(weather => weather.main.pressure)
 
     return (
       <tr key={ name }>
         <td>{ name }</td>
         <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
+          <Chart data={temps} color='orange' />
+        </td>
+        <td>
+          <Chart data={pressures} color='blue' />
         </td>
       </tr>
     )

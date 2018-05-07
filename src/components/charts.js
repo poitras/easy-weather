9a9cfbd33f47060235903e8dc4e02e8e55caca25
poitras <nicopoitras@gmail.com>
2018-05-06 23:30:25 -0400

    feat: humidity chart + average line + average number + units

--- src/components/charts.js
+++ src/components/charts.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { Sparklines, SparklinesLine } from 'react-sparklines'
+import React from "react"
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines'
+import _ from 'lodash'
 
+const average = (data) => {
+  return _.round(_.sum(data)/data.length)
+}
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
-      </Sparklines>
+        <SparklinesReferenceLine type="avg"/>
+      </Sparklines>{}
+      <div>{average(props.data)} {props.units}</div>
     </div>
   )
 }
